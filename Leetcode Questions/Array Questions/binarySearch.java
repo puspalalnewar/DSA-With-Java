59366e0b2532b646264ad034fc29ea746066c696
Puspalal <puspalalnewar79@gmail.com>
2024-09-18 23:39:44 +0530

    Solved Questions

--- Leetcode Questions/Array Questions/binarySearch.java
+++ Leetcode Questions/Array Questions/binarySearch.java
@@ -1,19 +1,21 @@
+import java.util.Scanner;
+
 public class binarySearch {
 
-    public static int binarySearch(int arr[] , int target){
-        
+    public static int binarySearch(int arr[], int target) {
+
         int left = 0;
-        int right = arr.length-1;
+        int right = arr.length - 1;
 
         while (left <= right) {
-            int mid = (right+left) / 2;
-            // int mid = left + (right - left) / 2;
-            if(arr[mid] == target){
+            // int mid = (right+left) / 2;
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
                 return mid;
             }
-            if(arr[mid] > target){
+            if (arr[mid] > target) {
                 right = mid - 1;
-            }else{
+            } else {
                 left = mid + 1;
             }
         }
@@ -21,8 +23,20 @@ public class binarySearch {
     }
 
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5, 6};
-        int target = 6;
-        System.out.println(binarySearch(arr, target));
+        Scanner sc = new Scanner(System.in);
+        int arr[] = { 1, 2, 3, 4, 5, 6 };
+        
+
+        for(int i = 0; i<10; i++){
+            System.out.println("Enter Target Element :");
+            int target = sc.nextInt();
+            System.out.println( "The target element is located at index : " + binarySearch(arr, target));
+            System.out.println("If You Want To Search Another Press 1. If You Don't Press 0");
+            int ur = sc.nextInt();
+            if(ur != 1){
+                break;
+            }
+        }
+        
     }
 }
---
+++ Leetcode Questions/Array Questions/findSmallest.java
@@ -0,0 +1,21 @@
+public class findSmallest {
+
+    public static int findSmallest(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int temp = arr[j];
+                if (arr[i] > arr[j]) {
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        return arr[0];
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 7, 0, 6, -1, 23 };
+        System.out.println("The smallest element : " + findSmallest(arr));
+
+    }
+}
