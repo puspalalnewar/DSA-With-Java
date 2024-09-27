49441035d5c39ff418766f9d0d458ce86abbc8d6
Puspalal <puspalalnewar79@gmail.com>
2024-09-27 20:50:54 +0530

    Solved singleNumberII

---
+++ Leetcode Questions/Array Questions/singleNumberII.java
@@ -0,0 +1,25 @@
+public class singleNumberII {
+    public static int singleNumber(int[] nums) {
+        int count = 0;
+        int store = 0;
+        for(int i = 0; i<nums.length; i++){
+            for(int j = 0; j<nums.length; j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                    store = nums[i];
+                }
+            }
+            if(count == 1){
+                return store;
+            }else{
+                count = 0;
+                store = 0;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 1, 2, 0};
+        System.out.println(singleNumber(arr));
+    }
+}
